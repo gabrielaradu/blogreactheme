56c03dbc873e9f10ce2f38d0ba35785c3e776898
Gabi <gabrielaradu.developer@gmail.com>
2020-05-19 12:53:04 +0300

    Add more demo content for a post.

--- src/components/posts/DetailsContent.js
+++ src/components/posts/DetailsContent.js
@@ -4,12 +4,96 @@ class DetailsContent extends Component {
     render() {
         return (
             <div>
-                Single post details here
-                {/*<h1 className="display-4 mobile-title">{this.props.title}</h1>*/}
-                {/*<p className="font-weight-lighter">*/}
-                {/*    {this.props.content}*/}
-                {/*</p>*/}
-                {/*<p className="date-title">{this.props.date}</p>*/}
+                <div className="row justify-content-center mb-5">
+                    <h1 className="display-4">This is a different post where you showcase something fun</h1>
+                </div>
+
+                <section className="font-weight-lighter">
+                    <h3>What is Lorem Ipsum?</h3>
+                    <br/>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the
+                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        type
+                        and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+                        the
+                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
+                        1960s
+                        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
+                        desktop
+                        publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <br/>
+                    <h3>Why do we use it?</h3>
+                    <br/>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page
+                        when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                        distribution of letters, as opposed to using 'Content here, content here', making it look like
+                        readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
+                        their
+                        default model text, and a search for 'lorem ipsum' will uncover many web sites still in their
+                        infancy. Various versions have evolved over the years, sometimes by accident, sometimes on
+                        purpose
+                        (injected humour and the like).</p>
+                    <br/>
+                    <h3>Where does it come from?</h3>
+                    <br/>
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin
+                        professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
+                        consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
+                        literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+                        1.10.33
+                        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
+                        This
+                        book is a treatise on the theory of ethics, very popular during the Renaissance. The first line
+                        of
+                        Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                    <br/>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                        Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced
+                        in
+                        their exact original form, accompanied by English versions from the 1914 translation by H.
+                        Rackham.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel diam ligula. Ut suscipit
+                        libero eget sem convallis, vel mollis tortor suscipit. Phasellus massa enim, commodo ac orci ut,
+                        consequat sagittis nulla. Maecenas eleifend felis in iaculis fringilla. In aliquet ullamcorper
+                        justo, vel egestas neque. In imperdiet felis ac porta malesuada. Nam sollicitudin, mauris id
+                        viverra lacinia, sapien lectus placerat nisl, non elementum elit justo vitae nisi. Quisque et
+                        sapien ut turpis dapibus finibus eu eu neque. In tincidunt a purus sit amet lacinia. Donec
+                        suscipit, odio vitae accumsan sagittis, metus mi eleifend sapien, ut blandit mauris orci in est.
+                        <br/>
+                        Nullam accumsan tortor dolor, sit amet fermentum leo scelerisque non. Phasellus a tristique
+                        neque, et tristique diam. Mauris eu dictum justo. Nullam non blandit lorem. Aenean scelerisque
+                        pharetra orci, in scelerisque dui luctus sed. Praesent mattis iaculis tortor quis iaculis.
+                        Pellentesque sodales neque ut volutpat efficitur. Donec quam nisi, aliquet vel nibh nec,
+                        fringilla fermentum libero. Quisque eleifend, lacus vitae laoreet gravida, tellus velit
+                        scelerisque diam, eget gravida lacus ex ac nunc. Curabitur et ultricies ligula. In vitae massa
+                        fermentum justo consequat consectetur. Mauris sed felis dolor. Class aptent taciti sociosqu ad
+                        litora torquent per conubia nostra, per inceptos himenaeos.
+                        <br/>
+                        Pellentesque at vestibulum odio, quis facilisis odio. Ut commodo libero nunc, eu imperdiet
+                        libero aliquam quis. Mauris fermentum libero augue, non porta tortor cursus vitae. Nunc
+                        fringilla sit amet orci non egestas. Nullam sed commodo magna. Orci varius natoque penatibus et
+                        magnis dis parturient montes, nascetur ridiculus mus. Pellentesque habitant morbi tristique
+                        senectus et netus et malesuada fames ac turpis egestas.
+                        <br/>
+                        Curabitur venenatis sit amet felis sed accumsan. Mauris ut quam a eros faucibus maximus.
+                        Vestibulum non tincidunt odio. Praesent id bibendum felis. Aenean risus nulla, lobortis eget
+                        eros sodales, laoreet pretium nisi. Fusce viverra diam eu metus tempor, sed ultrices lacus
+                        pulvinar. Phasellus eu nunc mi. Nunc eleifend lacus tortor, a scelerisque turpis interdum at.
+                        Etiam viverra placerat lacus eget volutpat. In lobortis tortor eu rutrum posuere. Phasellus
+                        pulvinar suscipit erat. Proin in posuere felis. Proin et urna hendrerit, tincidunt ligula
+                        pellentesque, eleifend metus. Sed urna risus, hendrerit a egestas in, interdum ut dolor. Nunc
+                        viverra venenatis lorem, vitae porta massa semper in.
+                        <br/>
+                        Integer pretium vitae dolor id euismod. Duis ultricies dolor vitae mollis accumsan. Nunc ac
+                        tortor vel dui congue mattis. Quisque mollis ipsum id ultricies ultrices. Aliquam vitae quam eu
+                        turpis lacinia hendrerit. Maecenas vel leo arcu. Cras sed nunc tortor. Maecenas consequat
+                        lacinia placerat. Fusce ac erat feugiat, sodales urna posuere, pretium nulla. </p>
+                </section>
+                <p className="date-title"></p>
             </div>
         );
     }
