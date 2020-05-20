6ced3a5ec2eb15075b827600c2174e601bad5a13
Gabi <gabrielaradu.developer@gmail.com>
2020-05-20 14:39:24 +0300

    Compiled the sources for production deployment.

---
+++ build/precache-manifest.3b50a698a140fa39c4a1b4afec5e3b7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe294a8934b9e03a0e12f059f6ba2cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c0767f82ece54870a9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cfdae2cd9728c48079be",
+    "url": "/static/css/main.0b8a9866.chunk.css"
+  },
+  {
+    "revision": "50c0767f82ece54870a9",
+    "url": "/static/js/2.d37b208c.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.d37b208c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdae2cd9728c48079be",
+    "url": "/static/js/main.d22b4a1d.chunk.js"
+  },
+  {
+    "revision": "1aa8c37c78877f250b41",
+    "url": "/static/js/runtime-main.958f608f.js"
+  },
+  {
+    "revision": "fc7840ce2390253d5fb8c46500104666",
+    "url": "/static/media/undraw_career.fc7840ce.svg"
+  },
+  {
+    "revision": "8f32ee992ba7a821c0ba1174894a498f",
+    "url": "/static/media/undraw_page_not_found.8f32ee99.svg"
+  }
+]);
